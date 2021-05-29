484425b261565d7109e32828e2854190baa7083b
srabanidutt <83363625+srabanidutt@users.noreply.github.com>
2021-05-29 11:25:08 +0530

    Add files via upload

---
+++ BasicLogicTest/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Routing from './Route/routing'
+function App() {
+  return (
+    <div className="App">
+        <Routing/>
+    </div>
+  );
+}
+
+export default App;
